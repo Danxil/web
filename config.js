84bc6dd4d9a689651a23308ac24c02d3a8fcc7e7
danxil <danxil@list.ru>
2015-01-17 12:23:48 +0200

    1

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ var app = express();
 
 module.exports.TEMP = process.env.TEMP_DIR || path.join(__dirname, 'tmp/');
 module.exports.CLOUD = process.env.CLOUD_DIR || path.join(__dirname, 'cloud/');
-module.exports.DB = app.get('env') == 'production' ? 'mongodb://danxil:7d1cE3a8@ds061189.mongolab.com:61189/name' :
+module.exports.DB = process.env.NODE_ENV == 'production' ? 'mongodb://danxil:7d1cE3a8@ds061189.mongolab.com:61189/name' :
                     'mongodb://localhost:27017/name';
